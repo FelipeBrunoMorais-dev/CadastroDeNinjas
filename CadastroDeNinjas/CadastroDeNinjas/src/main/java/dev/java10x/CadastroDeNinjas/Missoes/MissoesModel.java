349eb635e19ae8a54a2642f80b15b717f75edddd
Felipe Morais <felipe-bruno01@hotmail.com>
2026-08-19 00:11:33 -0300

    Modelagem do banco de dados, NinjaModel e MissoesModel Conluidos, anotation feita para relacionamento de tabelas

--- CadastroDeNinjas/CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ CadastroDeNinjas/CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,6 +3,8 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name="tb_missoes")
 public class MissoesModel {
@@ -12,6 +14,42 @@ public class MissoesModel {
     private Long id;
     private String nomedaMissao;
     private String dificuldade;
-    private NinjaModel ninja;
 
+    // OneToMany uma missão pode ter vario ninjas
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninja;
+
+    public MissoesModel(){
+
+    }
+
+    public MissoesModel(String nomedaMissao, String dificuldade, NinjaModel ninja) {
+        this.nomedaMissao = nomedaMissao;
+        this.dificuldade = dificuldade;
+        this.ninja = ninja;
+    }
+
+    public String getNomedaMissao() {
+        return nomedaMissao;
+    }
+
+    public void setNomedaMissao(String nomedaMissao) {
+        this.nomedaMissao = nomedaMissao;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public NinjaModel getNinja() {
+        return ninja;
+    }
+
+    public void setNinja(NinjaModel ninja) {
+        this.ninja = ninja;
+    }
 }
