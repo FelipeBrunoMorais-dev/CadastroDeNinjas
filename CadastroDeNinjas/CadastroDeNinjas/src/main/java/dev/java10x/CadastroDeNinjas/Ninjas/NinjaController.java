59edb59841ff96e53b73e509e284e9e1186815e8
Felipe Morais <felipe-bruno01@hotmail.com>
2026-08-28 00:04:55 -0300

    Feat: atualiza o controlle com mock de dados

--- CadastroDeNinjas/CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -8,11 +8,38 @@ import org.springframework.web.bind.annotation.*;
 public class NinjaController {
 
     @GetMapping("/boasVindas")
-    @PostMapping
-    @PutMapping
-    @PatchMapping
-    @DeleteMapping
-        public  String boasVindas(){
+         public  String boasVindas(){
             return "Essa é minha primeira mensagem nessa rota";
+        }
+
+        //adicionar ninja (create)
+        @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+        }
+
+
+    // mostrar todos os ninjas (read)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Mostrar todos os Ninjas";
+    }
+
+        // mostrar ninja por ID (read)
+    @GetMapping("/todosID")
+    public String mostrarNinjasID(){
+        return "Mostrar Ninja por ID";
+    }
+
+        // Alterar dados dos ninjas (update)
+    @PutMapping("/alterar")
+    public String alterarNinjasID() {
+        return "Alterar Ninja por ID";
+    }
+
+        // Deletar ninja (Delete)
+        @DeleteMapping("/deletarID")
+    public String deletarNinjasporID() {
+        return "Deletar Ninja por ID";
         }
 }
